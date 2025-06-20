84a1329d67777b625362fed31011cebe254b22e9
Maxi-Vergot <maxivergot@gmail.com>
2025-06-20 03:16:53 -0300

    Update con_embalaje_regalo.py
    import

--- sistema de pedidos/envios/con_embalaje_regalo.py
+++ sistema de pedidos/envios/con_embalaje_regalo.py
@@ -1,6 +1,6 @@
 
 
-from .envio_decorador import EnvioDecorador
+from envios.envio_decorador import EnvioDecorador
 
 class ConEmbalajeRegalo(EnvioDecorador):
     def __init__(self, envio_base, costo_embalaje=4.5):
